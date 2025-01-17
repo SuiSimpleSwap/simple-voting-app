0ecbdcb4fc7a1f8706b7a69be8f4eb152edb5539
Filip Jerga <jerga99@gmail.com>
2025-01-17 14:29:46 +0000

    Navigation Provider

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,24 @@
+import { useNavigation } from "../providers/navigation/NavigationContext";
 
 const Navbar = () => {
-  const currentPage: string = "/wallet";
+  const {currentPage, navigate} = useNavigation()
 
   return (
     <nav className="bg-gray-200 dark:bg-gray-800 p-4 shadow-md">
       <ul className="flex space-x-6">
         <li>
-          <button className={`px-4 py-2 rounded ${currentPage === "/" ? "bg-blue-400 underline" : ""}`}>
+          <button
+            onClick={() => navigate("/")}
+            className={`px-4 py-2 rounded ${currentPage === "/" ? "bg-blue-400 underline" : ""}`}
+          >
             Home
           </button>
         </li>
         <li>
-          <button className={`px-4 py-2 rounded ${currentPage === "/wallet" ? "bg-blue-400 underline" : ""}`}>
+          <button
+            onClick={() => navigate("/wallet")}
+            className={`px-4 py-2 rounded ${currentPage === "/wallet" ? "bg-blue-400 underline" : ""}`}
+          >
             Wallet
           </button>
         </li>
