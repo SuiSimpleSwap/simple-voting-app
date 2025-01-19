6f7ee0a995767164dfdbf816803d0f9c3ff15295
Filip Jerga <jerga99@gmail.com>
2025-01-19 11:39:08 +0000

    Wallet status

--- frontend/src/views/WalletView.tsx
+++ frontend/src/views/WalletView.tsx
@@ -1,29 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-
+import { WalletStatus } from "../components/wallet/Status";
 
 
 const WalletView = () => {
-    const { isPending, isFetching, data, error } = useQuery({
-      queryKey: ["gitQuery"],
-      queryFn: async () => {
-        const response = await fetch("https://api.github.com/repos/TanStack/query");
-        const data = await response.json();
-        return data;
-      }
-    });
-
-    if (isPending) return "Loading..."
-    if (error) return "An error occured: " + error.message;
-
     return (
       <>
         <div className="mb-8">
           <h1 className="text-4xl font-bold">Your Wallet Info</h1>
+          <WalletStatus />
         </div>
-        <h1>{data.full_name}</h1>
-        <p>{data.description}</p>
-        <strong>{data.forks_count}</strong>
-        <div>{isFetching ? "Updating" : ""}</div>
       </>
     )
 };
