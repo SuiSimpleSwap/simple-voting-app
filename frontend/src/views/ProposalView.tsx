37f49a8f4611c52b4b40e22dae44c1deea2e2369
Filip Jerga <jerga99@gmail.com>
2025-01-23 11:40:49 +0000

    Query to fetch dashboard

--- frontend/src/views/ProposalView.tsx
+++ frontend/src/views/ProposalView.tsx
@@ -1,28 +1,41 @@
+import { useSuiClientQuery } from "@mysten/dapp-kit";
 import { useNetworkVariable } from "../config/networkConfig";
 
-const PROPOSAL_COUNT = 7;
+// const PROPOSAL_COUNT = 7;
 
-const ProposalItem = () => {
-  return (
-    <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
-      <p className="text-xl font-semibold mb-2">Title: Hello There</p>
-      <p className="text-gray-700 dark:text-gray-300">Desc: What is your vote ?</p>
-    </div>
-  )
-}
+// const ProposalItem = () => {
+//   return (
+//     <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
+//       <p className="text-xl font-semibold mb-2">Title: Hello There</p>
+//       <p className="text-gray-700 dark:text-gray-300">Desc: What is your vote ?</p>
+//     </div>
+//   )
+// }
 
 const ProposalView = () => {
   const dashboardId = useNetworkVariable("dashboardId");
 
-  console.log(dashboardId);
+  const { data: dataResponse, isPending, error} = useSuiClientQuery(
+    "getObject", {
+      id: dashboardId,
+      options: {
+        showContent: true
+      }
+    }
+  );
+
+  if (isPending) return <div className="text-center text-gray-500">Loading...</div>;
+  if (error) return <div className="text-red-500">Error: {error.message}</div>;
+  if (!dataResponse.data) return <div className="text-center text-red-500">Not Found...</div>;
 
   return (
     <>
       <h1 className="text-4xl font-bold mb-8">New Proposals</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        { new Array(PROPOSAL_COUNT).fill(1).map((id) =>
+        {/* { new Array(PROPOSAL_COUNT).fill(1).map((id) =>
           <ProposalItem key={id * Math.random()}/>
-        )}
+        )} */}
+        {JSON.stringify(dataResponse)}
       </div>
     </>
   )
