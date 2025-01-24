5523fd325f5b47d8a4bdda1a50cd174ad128e85b
Filip Jerga <jerga99@gmail.com>
2025-01-24 08:39:34 +0000

    Parse proposal

--- frontend/src/components/proposal/ProposalItem.tsx
+++ frontend/src/components/proposal/ProposalItem.tsx
@@ -1,6 +1,8 @@
 import { useSuiClientQuery } from "@mysten/dapp-kit";
 import { FC } from "react";
 import { EcText } from "../Shared";
+import { SuiObjectData } from "@mysten/sui/client";
+import { Proposal } from "../../types";
 
 
 type ProposalItemsProps = {
@@ -21,12 +23,27 @@ export const ProposalItem: FC<ProposalItemsProps> = ({id}) => {
   if (error) return <EcText isError text={`Error: ${error.message}`}/>;
   if (!dataResponse.data) return <EcText text="Not Found"/>;
 
-  console.log(dataResponse.data);
+  const proposal = parseProposal(dataResponse.data);
+
+  if (!proposal) return <EcText text="No data found!"/>
 
   return (
     <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
-      <p className="text-xl font-semibold mb-2">Title: {id}</p>
-      <p className="text-gray-700 dark:text-gray-300">Desc: What is your vote ?</p>
+      <p className="text-xl font-semibold mb-2">{proposal.title}</p>
+      <p className="text-gray-700 dark:text-gray-300">{proposal.description}</p>
     </div>
   )
 }
+
+function parseProposal(data: SuiObjectData): Proposal | null {
+  if (data.content?.dataType !== "moveObject") return null;
+
+  const { voted_yes_count, voted_no_count, expiration, ...rest } = data.content.fields as any;
+
+  return {
+    ...rest,
+    votedYesCount: Number(voted_yes_count),
+    votedNoCount: Number(voted_no_count),
+    expiration: Number(expiration)
+  };
+ }
---
+++ frontend/src/types.ts
@@ -0,0 +1,12 @@
+
+
+export type Proposal = {
+  id: SuiID,
+  title: string,
+  description: string,
+  votedYesCount: string,
+  votedNoCount: string,
+  expiration: number,
+  creator: string,
+  voter_registry: string[]
+};
