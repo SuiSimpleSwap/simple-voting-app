ab94c0103cae0078be71a94e179a712faad00392
Filip Jerga <jerga99@gmail.com>
2025-01-26 10:47:24 +0000

    Vote modal

--- frontend/src/components/proposal/ProposalItem.tsx
+++ frontend/src/components/proposal/ProposalItem.tsx
@@ -1,8 +1,9 @@
 import { useSuiClientQuery } from "@mysten/dapp-kit";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { EcText } from "../Shared";
 import { SuiObjectData } from "@mysten/sui/client";
 import { Proposal } from "../../types";
+import { VoteModal } from "./VoteModal";
 
 
 type ProposalItemsProps = {
@@ -10,6 +11,7 @@ type ProposalItemsProps = {
 };
 
 export const ProposalItem: FC<ProposalItemsProps> = ({id}) => {
+  const [isModelOpen, setIsModelOpen] = useState(false);
   const { data: dataResponse, error, isPending} = useSuiClientQuery(
     "getObject", {
       id,
@@ -28,25 +30,34 @@ export const ProposalItem: FC<ProposalItemsProps> = ({id}) => {
   if (!proposal) return <EcText text="No data found!"/>
 
   return (
-    <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
-      <p className="text-xl font-semibold mb-2">{proposal.title}</p>
-      <p className="text-gray-700 dark:text-gray-300">{proposal.description}</p>
-      <div className="flex items-center justify-between mt-4">
-        <div className="flex space-x-4">
-          <div className="flex items-center text-green-600">
-            <span className="mr-1">👍</span>
-            {proposal.votedYesCount}
+    <>
+      <div
+        onClick={() => setIsModelOpen(true)}
+        className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors"
+      >
+        <p className="text-xl font-semibold mb-2">{proposal.title}</p>
+        <p className="text-gray-700 dark:text-gray-300">{proposal.description}</p>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex space-x-4">
+            <div className="flex items-center text-green-600">
+              <span className="mr-1">👍</span>
+              {proposal.votedYesCount}
+            </div>
+            <div className="flex items-center text-red-600">
+              <span className="mr-1">👎</span>
+              {proposal.votedNoCount}
+            </div>
           </div>
-          <div className="flex items-center text-red-600">
-            <span className="mr-1">👎</span>
-            {proposal.votedNoCount}
+          <div>
+            <EcText text={formatUnixTime(proposal.expiration)}/>
           </div>
-        </div>
-        <div>
-          <EcText text={formatUnixTime(proposal.expiration)}/>
         </div>
       </div>
-    </div>
+      <VoteModal
+        isOpen={isModelOpen}
+        onClose={() => setIsModelOpen(false)}
+      />
+    </>
   )
 }
 
