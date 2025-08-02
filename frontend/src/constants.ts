f76a496bdbe68e8f0120515dfcff9189ada225a1
“marvel” <marvelmichael67@gmail.com>
2025-08-02 11:11:52 +0700

    init

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,16 +1,19 @@
-
-export const TESTNET_PACKAGE_ID = "0x058171d13219e409423faf756b48abbe6f50c6f902e5ba39efa3b7524321b9f6";
-export const DEVNET_PACKAGE_ID = "0x95120f4f689ef4b8056c286373480dd84401a960794f6891585c1777b890cb61";
-export const LOCAL_PACKAGE_ID = "0x01fa087b355286482242d897e009141677dd7e66683de36a836ea3410e877090";
+export const TESTNET_PACKAGE_ID =
+  "0x99b40df339058b7f6eb31c1ad1e22a2ad6c4ec9ee170e75716ae5fa65b093e9d";
+export const DEVNET_PACKAGE_ID = "";
+export const LOCAL_PACKAGE_ID = "";
 export const MAINNET_PACKAGE_ID = "0x234";
 
-export const TESTNET_ADMIN_CAP = "0x7c09192cf36d236fc0d8c0885ae6e20c58644fceff8d72a3b2cc7285dc3cbedf";
-export const DEVNET_ADMIN_CAP = "0xfe0c04b2708fd4c6c72f24eacd229542abbff7481beca4d0475c4589e36b5ccd";
-export const LOCAL_ADMIN_CAP = "0xac2cfc37c1e540bfac3f13db201b16f8f518829f14a0db7c3d3d2841415eda2b";
+// Menambahkan ID objek dan informasi terkait dari transaksi yang diberikan
+export const TESTNET_ADMIN_CAP =
+  "0xe28d912d31d650333cf8b96b48e378fd2fc007dc99955bc25e6d74105e9bb481";
+export const DEVNET_ADMIN_CAP = "";
+export const LOCAL_ADMIN_CAP = "";
 export const MAINNET_ADMIN_CAP = "0x234";
 
-export const TESTNET_DASHBOARD_ID = "0x592fc09dddf7217aa3e47471fc72aae92760c890a82056dff0be818b8e73f8f0";
-export const DEVNET_DASHBOARD_ID = "0xe6fecf3c4d8e9a61923a9bce261f1b7ffdff414d6e2231e50d0ceb67fcb4d63f";
-export const LOCAL_DASHBOARD_ID = "0x51238d0bf9f360876237ea8ff5387644dbcab600adbc2d8018b0d308afa59f05";
-export const MAINNET_DASHBOARD_ID= "0x234";
-
+// Menggunakan informasi dari objek yang dibuat pada transaksi
+export const TESTNET_DASHBOARD_ID =
+  "0x01a01706fcf8723c1bc0d06f7a234942106da392a049e7ae9ef52b6461d59cc7";
+export const DEVNET_DASHBOARD_ID = "";
+export const LOCAL_DASHBOARD_ID = "";
+export const MAINNET_DASHBOARD_ID = "0x234";
